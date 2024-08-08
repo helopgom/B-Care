048547349467e256ac14554779399489556e1f12
helopgom <helena.lopez.gomez@gmail.com>
2024-08-08 16:21:14 +0200

    installation react router dom

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,11 +1,8 @@
-import Navbar from "../components/navbar/Navbar";
 import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
     <div>
-      <Navbar />
-
       <main>
         <Outlet />
       </main>
