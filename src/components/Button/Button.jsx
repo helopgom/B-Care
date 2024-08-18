a75c8f74c3d6466b1b9490b8e8f3b9caf91e2ca8
helopgom <helena.lopez.gomez@gmail.com>
2024-08-18 21:10:59 +0200

    Add finish button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import "./Button.css";
 
-const Button = ({ text, backgroundColor, textColor, borderColor, url }) => {
+const Button = ({
+  text,
+  backgroundColor,
+  textColor,
+  borderColor,
+  url,
+  onClick,
+}) => {
   return (
     <button
       className="custom-button"
-      style={{
-        backgroundColor: backgroundColor,
-        color: textColor,
-        borderColor: borderColor || "transparent",
-      }}
-      onClick={() => (window.location.href = url)}
+      style={{ backgroundColor, color: textColor, borderColor }}
+      onClick={onClick ? onClick : () => (window.location.href = url)}
     >
       {text}
     </button>
--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -7,7 +7,7 @@ import ButtonWithIcon from "../../components/ButtonWithIcon/ButtonWithIcon";
 
 const Home = () => {
   const [name, setName] = useState("");
-  const [isTalking, setIsTalking] = useState(false); // Estado movido a Home
+  const [isTalking, setIsTalking] = useState(false);
 
   useEffect(() => {
     const fetchName = async () => {
@@ -18,11 +18,15 @@ const Home = () => {
     fetchName();
   }, []);
 
+  const handleFinish = () => {
+    setIsTalking(false); // Finaliza el estado de "Hablando"
+  };
+
   return (
     <div className="home-container">
       <InteractiveText name={name} isTalking={isTalking} />
       <ButtonWithIcon isTalking={isTalking} setIsTalking={setIsTalking} />
-      {/* Renderizar condicionalmente el botón */}
+      {/* Renderizar condicionalmente el botón "MI CUENTA" */}
       {!isTalking && (
         <div className="button-container">
           <Button
@@ -31,6 +35,19 @@ const Home = () => {
             textColor="var(--black)"
             borderColor="var(--black)"
             url="#"
+          />
+        </div>
+      )}
+      {/* Renderizar condicionalmente el botón "Finalizar" */}
+      {isTalking && (
+        <div className="finish-button-container">
+          <Button
+            text="FINALIZAR"
+            backgroundColor="var(--black)" // Ajusta el color según sea necesario
+            textColor="var(--white)"
+            borderColor="var(--black)"
+            url="#"
+            onClick={handleFinish}
           />
         </div>
       )}
